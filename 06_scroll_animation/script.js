1d9a369436a27c3581a8b7019dd4ca3e78a70061
gunkarlsson <hi@gunkarlsson.com>
2021-01-10 19:09:48 +0100

    ✨  day 6, scroll animation

---
+++ 06_scroll_animation/script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 100) * 85 + window.innerHeight;
+
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
