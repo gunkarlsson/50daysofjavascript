4674c8b1c60a707a8d2f0b12645b0e888fa28cce
gunkarlsson <hi@gunkarlsson.com>
2021-01-13 14:18:28 +0100

    ✨  day 9, sound board

---
+++ 09_sound_board/script.js
@@ -0,0 +1,25 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerHTML = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    const soundElement = document.getElementById(sound);
+    soundElement.volume = 0.2;
+    soundElement.play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+
+    song.pause();
+    song.currentTime = 0;
+  });
+}
